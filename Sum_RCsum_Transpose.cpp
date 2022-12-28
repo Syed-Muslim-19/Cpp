0427c4d0a24dddc33fb7f1e0f96729a12818ec3d
Syed M Muslim <117216018+Syed-Muslim-19@users.noreply.github.com>
2022-12-28 14:39:21 +0500

    Update Sum_RCsum_Transpose.cpp

--- Sum_RCsum_Transpose.cpp
+++ Sum_RCsum_Transpose.cpp
@@ -1,77 +1,77 @@
-//#include<iostream>
-//using namespace std;
-//int main()
-//{
-//	int m, n, sumr = 0, sumc = 0, fsum = 0;
-//	int arr[100][100];
-//	cout << "Enter the rows : ";
-//	cin >> m;
-//	cout << "Enter the cloumns : ";
-//	cin >> n;
-//	for (int i = 0; i < m; i++)
-//	{
-//		int j;
-//		for (j = 0; j < n; j++)
-//		{
-//			cin >> arr[i][j];
-//		}
-//	}
-//	cout << "The Matrice is : " << endl;
-//	for (int i = 0; i < m; i++)
-//	{
-//		int j;
-//		for (j = 0; j < n; j++)
-//		{
-//			cout << arr[i][j] << " ";
-//		}
-//		cout << endl;
-//	}
-//	for (int i = 0; i < m; i++)
-//	{
-//	    int j;
-//		for (j = 0; j < n; j++)
-//		{
-//			sumr +=arr[i][j];
-//		}
-//	}
-//	cout << "Sum of the elements of Matrice is : " <<sumr<<endl;
-//	sumr = 0;
-//	cout << endl;
-//	cout << "Sum of Rows : " << endl;
-//	for (int i = 0; i < m; i++)
-//	{
-//		int j;
-//		for (j = 0; j < n; j++)
-//		{
-//			sumr += arr[i][j];
-//		}
-//		fsum = sumr - sumc;
-//		cout << "Element of row " << i + 1 << " sum is : " << fsum<<endl;
-//		sumr=sumc;
-//	}
-//	cout << endl;
-//	cout << "Sum of Columns : " << endl;
-//	for (int i = 0; i < n; i++)
-//	{
-//		int j;
-//		for (j = 0; j < m; j++)
-//		{
-//		sumr += arr[j][i];
-//		}
-//		fsum = sumr - sumc;
-//		cout << "Element of Column " << i + 1 << " sum is : " << fsum << endl;
-//		sumr = sumc;
-//	}
-//	cout << endl;
-//	cout << "Transpose of Matric : " << endl;
-//	for (int i = 0; i < n; i++)
-//	{
-//		int j;
-//		for (j = 0; j < m; j++)
-//		{
-//			cout << arr[j][i] << " ";
-//		}
-//		cout << endl;
-//	}
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+int main()
+{
+	int m, n, sumr = 0, sumc = 0, fsum = 0;
+	int arr[100][100];
+	cout << "Enter the rows : ";
+	cin >> m;
+	cout << "Enter the cloumns : ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+	{
+		int j;
+		for (j = 0; j < n; j++)
+		{
+			cin >> arr[i][j];
+		}
+	}
+	cout << "The Matrice is : " << endl;
+	for (int i = 0; i < m; i++)
+	{
+		int j;
+		for (j = 0; j < n; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < m; i++)
+	{
+	    int j;
+		for (j = 0; j < n; j++)
+		{
+			sumr +=arr[i][j];
+		}
+	}
+	cout << "Sum of the elements of Matrice is : " <<sumr<<endl;
+	sumr = 0;
+	cout << endl;
+	cout << "Sum of Rows : " << endl;
+	for (int i = 0; i < m; i++)
+	{
+		int j;
+		for (j = 0; j < n; j++)
+		{
+			sumr += arr[i][j];
+		}
+		fsum = sumr - sumc;
+		cout << "Element of row " << i + 1 << " sum is : " << fsum<<endl;
+		sumr=sumc;
+	}
+	cout << endl;
+	cout << "Sum of Columns : " << endl;
+	for (int i = 0; i < n; i++)
+	{
+		int j;
+		for (j = 0; j < m; j++)
+		{
+		sumr += arr[j][i];
+		}
+		fsum = sumr - sumc;
+		cout << "Element of Column " << i + 1 << " sum is : " << fsum << endl;
+		sumr = sumc;
+	}
+	cout << endl;
+	cout << "Transpose of Matric : " << endl;
+	for (int i = 0; i < n; i++)
+	{
+		int j;
+		for (j = 0; j < m; j++)
+		{
+			cout << arr[j][i] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
