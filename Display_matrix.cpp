a297fb64cd51fb246e736bebb00103ccf9087cf7
Syed M Muslim <117216018+Syed-Muslim-19@users.noreply.github.com>
2022-12-28 14:35:28 +0500

    Update Display_matrix.cpp

--- Display_matrix.cpp
+++ Display_matrix.cpp
@@ -1,30 +1,30 @@
-//#include<iostream>
-//using namespace std;
-//int main()
-//{
-//	int m, n;
-//	int arr[100][100];
-//	cout << "Enter the rows : ";
-//	cin >> m;
-//	cout << "Enter the cloumns : ";
-//	cin >> n;
-//	for (int i = 0; i < m; i++)
-//	{
-//		int j;
-//		for (j = 0; j < n; j++)
-//		{
-//			cin >> arr[i][j];
-//		}
-//	}
-//	cout << "The Matrice is : " << endl;
-//	for (int i = 0; i < m; i++)
-//	{
-//		int j;
-//		for (j = 0; j < n; j++)
-//		{
-//			cout<<arr[i][j]<<" ";
-//		}
-//		cout << endl;
-//	}
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+int main()
+{
+	int m, n;
+	int arr[100][100];
+	cout << "Enter the rows : ";
+		cin >> m;
+		cout << "Enter the cloumns : ";
+		cin >> n;
+		for (int i = 0; i < m; i++)
+		{
+			int j;
+			for (j = 0; j < n; j++)
+			{
+				cin >> arr[i][j];
+			}
+		}
+		cout << "The Matrice is : " << endl;
+		for (int i = 0; i < m; i++)
+		{
+			int j;
+			for (j = 0; j < n; j++)
+			{
+				cout<<arr[i][j]<<" ";
+			}
+			cout << endl;
+		}
+		return 0;
+	}
