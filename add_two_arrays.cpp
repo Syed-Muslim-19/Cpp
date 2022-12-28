7de686929fb2f6fcc7c8161c67565c39aaa74e3b
Syed M Muslim <117216018+Syed-Muslim-19@users.noreply.github.com>
2022-12-28 14:43:51 +0500

    Update add_two_arrays.cpp

--- add_two_arrays.cpp
+++ add_two_arrays.cpp
@@ -1,62 +1,62 @@
-//#include<iostream>
-//using namespace std;
-//int main()
-//{
-//	int m, n;
-//	int arr1[100][100], arr2[100][100];
-//	cout << "Enter the rows : ";
-//	cin >> m;
-//	cout << "Enter the cloumns : ";
-//	cin >> n;
-//	cout << "Array One Element : "<<endl;
-//	for (int i = 0; i < m; i++)
-//	{
-//		int j;
-//		for (j = 0; j < n; j++)
-//		{
-//			cin >> arr1[i][j];
-//		}
-//	}
-//	cout << "Array One Element : "<<endl;
-//	for (int i = 0; i < m; i++)
-//	{
-//		int j;
-//		for (j = 0; j < n; j++)
-//		{
-//			cin >> arr2[i][j];
-//		}
-//	}
-//	cout << "The Matrice one is : " << endl;
-//	for (int i = 0; i < m; i++)
-//	{
-//		int j;
-//		for (j = 0; j < n; j++)
-//		{
-//			cout<<arr1[i][j]<<" ";
-//		}
-//		cout << endl;
-//	}
-//	cout << endl;
-//	cout << "The Matrice Two is : " << endl;
-//	for (int i = 0; i < m; i++)
-//	{
-//		int j;
-//		for (j = 0; j < n; j++)
-//		{
-//			cout << arr2[i][j] << " ";
-//		}
-//		cout << endl;
-//	}
-//	cout << endl;
-//	cout << "Sum of two arrays is : " << endl;
-//	for (int i = 0; i < m; i++)
-//	{
-//		int j;
-//		for (j = 0; j < n; j++)
-//		{
-//			cout << arr1[i][j]+arr2[i][j] << " ";
-//		}
-//		cout << endl;
-//	}
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+int main()
+{
+	int m, n;
+	int arr1[100][100], arr2[100][100];
+	cout << "Enter the rows : ";
+	cin >> m;
+	cout << "Enter the cloumns : ";
+	cin >> n;
+	cout << "Array One Element : "<<endl;
+	for (int i = 0; i < m; i++)
+	{
+		int j;
+		for (j = 0; j < n; j++)
+		{
+			cin >> arr1[i][j];
+		}
+	}
+	cout << "Array One Element : "<<endl;
+	for (int i = 0; i < m; i++)
+	{
+		int j;
+		for (j = 0; j < n; j++)
+		{
+			cin >> arr2[i][j];
+		}
+	}
+	cout << "The Matrice one is : " << endl;
+	for (int i = 0; i < m; i++)
+	{
+		int j;
+		for (j = 0; j < n; j++)
+		{
+			cout<<arr1[i][j]<<" ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	cout << "The Matrice Two is : " << endl;
+	for (int i = 0; i < m; i++)
+	{
+		int j;
+		for (j = 0; j < n; j++)
+		{
+			cout << arr2[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	cout << "Sum of two arrays is : " << endl;
+	for (int i = 0; i < m; i++)
+	{
+		int j;
+		for (j = 0; j < n; j++)
+		{
+			cout << arr1[i][j]+arr2[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
