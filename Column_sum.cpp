9f6f7b2e023706ccfb48d0a1e349e2a4697637f9
Syed M Muslim <117216018+Syed-Muslim-19@users.noreply.github.com>
2022-12-28 14:36:18 +0500

    Update Column_sum.cpp

--- Column_sum.cpp
+++ Column_sum.cpp
@@ -1,43 +1,43 @@
-//#include<iostream>
-//using namespace std;
-//int main()
-//{
-//	int m, n, sumr = 0, sumc = 0, fsum = 0;
-//	int arr[100][100];
-//	cout << "Enter the rows : ";
-//	cin >> m;
-//	cout << "Enter the cloumns : ";
-//	cin >> n;
-//	for (int i = 0; i < m; i++)
-//	{
-//		int j;
-//		for (j = 0; j < n; j++)
-//		{
-//			cin >> arr[i][j];
-//		}
-//	}
-//	cout << "The Matrice is : " << endl;
-//	for (int i = 0; i < m; i++)
-//	{
-//		int j;
-//		for (j = 0; j < n; j++)
-//		{
-//			cout << arr[i][j] << " ";
-//		}
-//		cout << endl;
-//	}
-//	cout << endl;
-//	for (int i = 0; i < n; i++)
-//	{
-//		int j;
-//		for (j = 0; j < m; j++)
-//		{
-//		sumr += arr[j][i];
-//		}
-//		fsum = sumr - sumc;
-//		cout << "Element of Column " << i + 1 << " sum is : " << fsum << endl;
-//		sumr = sumc;
-//	}
-//
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+int main()
+{
+	int m, n, sumr = 0, sumc = 0, fsum = 0;
+	int arr[100][100];
+	cout << "Enter the rows : ";
+	cin >> m;
+	cout << "Enter the cloumns : ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+	{
+		int j;
+		for (j = 0; j < n; j++)
+		{
+			cin >> arr[i][j];
+		}
+	}
+	cout << "The Matrice is : " << endl;
+	for (int i = 0; i < m; i++)
+	{
+		int j;
+		for (j = 0; j < n; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	for (int i = 0; i < n; i++)
+	{
+		int j;
+		for (j = 0; j < m; j++)
+		{
+		sumr += arr[j][i];
+		}
+		fsum = sumr - sumc;
+		cout << "Element of Column " << i + 1 << " sum is : " << fsum << endl;
+		sumr = sumc;
+	}
+
+	return 0;
+}
